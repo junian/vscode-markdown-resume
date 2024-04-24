b980cdf9b6e9bc3417f3cdead223b15f2947eca0
Junian Triajianto <author@junian.dev>
2024-04-24 14:55:20 +0700

    Add commands

--- src/extension.ts
+++ src/extension.ts
@@ -8,18 +8,32 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-markdown-resume" is now active!');
+	//console.log('Congratulations, your extension "vscode-markdown-resume" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-markdown-resume.helloWorld', () => {
+	let createNew = vscode.commands.registerCommand('vscode-markdown-resume.createNew', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from vscode-markdown-resume!');
 	});
 
-	context.subscriptions.push(disposable);
+	let preview = vscode.commands.registerCommand('vscode-markdown-resume.preview', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from vscode-markdown-resume!');
+	});
+
+	let createPdf = vscode.commands.registerCommand('vscode-markdown-resume.createPdf', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from vscode-markdown-resume!');
+	});
+
+	context.subscriptions.push(createNew);
+	context.subscriptions.push(preview);
+	context.subscriptions.push(createPdf);
 }
 
 // This method is called when your extension is deactivated
